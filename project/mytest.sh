f21cffb07288adab2e6e79f78620fefd28e45820
emeka <bredemi@yahoo.com>
2018-04-13 19:09:04 -0500

    m1

--- project/mytest.sh
+++ project/mytest.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
-FROM jenkins/jenkins:lts
-USER root
-RUN apt-get update
-RUN apt-get install -y \
-apt-transport-https \
-ca-certificates \
-curl \
-gnupg2 \
-software-properties-common
-RUN curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-RUN add-apt-repository \
-"deb [arch=amd64] https://download.docker.com/linux/debian \
-$(lsb_release -cs) \
-stable"
-RUN apt-get update
-RUN apt-get install -y docker-ce
+docker build -t myjenk .
+docker run -itd -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v ~/tmp/jenkins/jenkins_home:/var/jenkins_home --name jenkins myjenk
